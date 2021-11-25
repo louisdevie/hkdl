c607bb3a79b0cf8c8db8286c117cf1fdb87d3d01
Louis DEVIE <louisdevie.contact@gmail.com>
2021-11-25 10:40:31 +0100

    test

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-"""Flask App Project."""
-
 from flask import Flask, jsonify
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
+    return "Hello, World!"
 
 
 if __name__ == '__main__':

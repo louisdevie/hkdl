2b74093f4923b46b572dc26f32d638e830238fb2
Louis DEVIE <louisdevie.contact@gmail.com>
2021-11-25 11:47:12 +0100

    added error pages

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.errorhandler(403)
+def not_found(e):
+    return render_template('403.html'), 403
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def not_found(e):
+    return render_template('500.html'), 500
+
+@app.route('/embed/<link>')
+def downloader(link):
+    return link
 
 if __name__ == '__main__':
     app.run()
